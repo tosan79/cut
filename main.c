0db9690032c88f8f6573509f6507ea4eb602af2c
tosan79 <1997ak@gmail.com>
2023-01-17 17:23:03 +0100

    first try to write Reader

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* fp = fopen("/proc/stat", "r");
+    FILE* fp2 = fopen("./out", "w+");
+    char c = fgetc(fp);
+    while (c != EOF) {
+        fputc(c, fp2);
+        c = fgetc(fp);
+        //printf("%c", c);
+    }
+    fclose(fp);
+    fclose(fp2);
+}
